c92fb220489daf01fee1c28798ca272ec74ceeb4
Harsh08Singh <99711903+Harsh08Singh@users.noreply.github.com>
2024-06-19 19:23:29 +0530

    improved performance

--- app/pubsub.pubnub.js
+++ app/pubsub.pubnub.js
@@ -1,10 +1,6 @@
 const PubNub = require('pubnub');
 
-const credentials = {
-  publishKey: 'pub-c-ec30f7ec-578f-4aa2-81c8-59077fb942c4',
-  subscribeKey: 'sub-c-eda4e664-027b-11e9-a39c-e60c31199fb2',
-  secretKey: 'sec-c-OWQwMTg1MGMtY2U2YS00ZmVlLWE1YmEtOTVmMWZmN2ZiOWVm'
-};
+const credentials = {};
 
 const CHANNELS = {
   TEST: 'TEST',
